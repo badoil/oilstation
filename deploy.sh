38e8f2eb4f07d8068651a85e7eb526512dcd415e
조건우 <gunwbro@gmail.com>
2021-01-22 11:06:13 +0900

    자동배포 테스트

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,6 @@ echo "[$(date)] oil deploy" >> /home/ubuntu/deploy.log
 sudo cp -rf /home/ubuntu/deploy/* /home/ubuntu/oilstation
 sudo cp -f /home/ubuntu/.env /home/ubuntu/oilstation
 
+cd /home/ubuntu/.env /home/ubuntu/oilstation
+sudo npm install --force
 sudo pm2 reload oil
