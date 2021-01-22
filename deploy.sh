b867d0bcde753ef6c9862fdffbdc5171fe1a9529
조건우 <gunwbro@gmail.com>
2021-01-22 13:51:32 +0900

    deploy 에 iptable 추가

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,7 @@
 echo "[$(date)] oil deploy" >> /home/ubuntu/deploy.log
 sudo cp -rf /home/ubuntu/deploy/* /home/ubuntu/oilstation
 sudo cp -f /home/ubuntu/.env /home/ubuntu/oilstation
+sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 3000
 
+cd /home/ubuntu/oilstation
 sudo pm2 start --name oil npm -- run start:prod
