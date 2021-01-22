4f3123562330310eb6b65aee2c70d35f85df3f2d
조건우 <gunwbro@gmail.com>
2021-01-22 13:26:34 +0900

    deploy 재수정

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ echo "[$(date)] oil deploy" >> /home/ubuntu/deploy.log
 sudo cp -rf /home/ubuntu/deploy/* /home/ubuntu/oilstation
 sudo cp -f /home/ubuntu/.env /home/ubuntu/oilstation
 
-sudo pm2 reload oil
+sudo pm2 start --name oil npm -- run start:prod
