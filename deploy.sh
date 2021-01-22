060f20e49a045a7409b2dff14afb935c186fb6af
조건우 <gunwbro@gmail.com>
2021-01-22 11:23:58 +0900

    start:prod에 sudo 추가

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,5 @@ sudo cp -rf /home/ubuntu/deploy/* /home/ubuntu/oilstation
 sudo cp -f /home/ubuntu/.env /home/ubuntu/oilstation
 
 cd /home/ubuntu/.env /home/ubuntu/oilstation
-sudo npm install --force
+
 sudo pm2 reload oil
